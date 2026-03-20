0eeb107d9b44b93dd63f6b8e35be6c0361e9ed48
s111410505 <rojine921@gmail.com>
2026-03-20 12:30:30 +0800

    Create 06.js

---
+++ homework/04/06.js
@@ -0,0 +1,12 @@
+let student = {
+    name: "小明",
+    age: 18,
+    score: 90
+};
+
+console.log(student.name);
+console.log(student.score);
+
+PS C:\Users\user\html2server\_wp\homework\04> node 6-score.js
+小明
+90
