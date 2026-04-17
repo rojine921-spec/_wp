556728a67bf8e86e7357659f656d8e58f66fb174
rojine921-spec <rojine921@gmail.com>
2026-04-17 10:34:14 +0800

    fix conflict

--- homework/06/01.js
+++ homework/06/01.js
@@ -2,22 +2,12 @@ function mathTool(num1, num2, action) {
   return action(num1, num2);
 }
 
-<<<<<<< HEAD
 // 相加
-console.log(mathTool(10, 5, function(a, b) {
-  return a + b;
-})); // 15
-
-// 相減
-console.log(mathTool(10, 5, function(a, b) {
-  return a - b;
-})); // 5
-=======
 console.log(mathTool(10, 5, function(a, b) {
   return a + b;
 }));
 
+// 相減
 console.log(mathTool(10, 5, function(a, b) {
   return a - b;
-})); 
->>>>>>> b6da412fc18df30f0e31486870d6b9aa45081e80
+}));
